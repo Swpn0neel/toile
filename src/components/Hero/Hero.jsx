6ec248d8b1c33e2cc9a473e8d81a87a36662fa4b
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-10-25 21:42:27 +0530

    (fix): Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,19 +4,19 @@ import "./Hero.css";
 export default function Hero(){
     return(
         <>
-            <div class="grid grid-cols-6">
-                <div class="pl-20 pt-60 pb-16 text-white text-6xl custom2 col-span-4 flex flex-col  gap-4">
+            <div class="flex flex-rows">
+                <div class="pl-20 pt-60 pb-16 text-white text-6xl custom2 basis-2/3 flex flex-col gap-4">
                     <div>The Magic</div>
                     <div>you need the most</div>
                 </div>
-                <div class="pt-60 custom1 col-span-2 justify-center"></div>
+                <div class="basis-1/3 custom1 justify-center"></div>
             </div>
-            <div class="grid grid-cols-6">
-                <div class="p-20 custom2 text-[#D9D9D950] col-span-4 flex justify-center">
-                    SOMETIMES IT’S JUST OUR FEELINGS THAT GET INVOLVED IN BETWEEN OUR HEART AND MIND, AND WE CAN’T TAKE THE DECISION WHICH IS RIGHT FOR US...
+            <div class="flex flex-rows">
+                <div class="px-20 basis-2/3 custom2 text-[#D9D9D950] flex justify-center items-center">
+                SOMETIMES IT’S JUST OUR FEELINGS THAT GET INVOLVED IN BETWEEN OUR HEART AND MIND, AND WE CAN’T TAKE THE DECISION WHICH IS RIGHT FOR US...
                 </div>
-                <div class="py-20 custom2 justify-center"></div>
-                <div class="py-20 custom1 justify-center"></div>
+                <div class="basis-1/6 py-20 custom2 justify-center"></div>
+                <div class="basis-1/6 py-20 custom1 justify-center"></div>
             </div>
         </>
     );
