665f81219116fd65cda135b506b3a293a271fd75
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-10-25 21:33:43 +0530

    (fix): Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import "./Navbar.css";
 export default function Navbar(){
     return(
         <>
-            <div class="grid grid-cols-6">
-                <div class="py-8 custom2 bg-[#d9d9d9] text-center">IMG</div>
-                <div class="py-8 custom2 text-[#D9D9D950] col-span-3 flex justify-center">
+            <div class="flex flex-rows h-24">
+                <div class="basis-1/6 custom2 bg-[#d9d9d9] flex justify-center items-center">IMG</div>
+                <div class="basis-1/2 custom2 text-[#D9D9D950] flex justify-center items-center">
                     <div class="pr-10">HOME</div>
                     <div class="px-10">ABOUT</div>
                     <div class="pl-10">GALLERY</div>
                 </div>
-                <div class="py-8 custom2 text-[#D9D9D950] text-center">ADD+</div>
-                <div class="py-8 custom1 text-white text-center">SIGN UP</div>
+                <div class="basis-1/6 custom2 text-[#D9D9D950] flex justify-center items-center">ADD+</div>
+                <div class="basis-1/6 custom1 text-white flex justify-center items-center">SIGN UP</div>
             </div>
         </>
     );
