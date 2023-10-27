c68a71b03603b7dbc663f005212683e9dc0e5c4a
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-10-27 21:17:41 +0530

    (feat): Added Custom Cursor

---
+++ src/components/Cursor/Cursor.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { gsap } from "gsap";
+import "./Cursor.css";
+
+export default function About() {
+  useEffect(() => {
+    const $bigBall = document.querySelector('.cursor__ball--big');
+    const $smallBall = document.querySelector('.cursor__ball--small');
+    const $hoverables = document.querySelectorAll('.hoverable');
+
+    // Listeners
+    document.body.addEventListener('mousemove', onMouseMove);
+    for (let i = 0; i < $hoverables.length; i++) {
+      $hoverables[i].addEventListener('mouseenter', onMouseHover);
+      $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+    }
+
+    // Move the cursor
+    function onMouseMove(e) {
+      gsap.to($bigBall, {
+        duration: 0.4,
+        x: e.pageX - 15,
+        y: e.pageY - 15
+      });
+      gsap.to($smallBall, {
+        duration: 0.1,
+        x: e.pageX - 5,
+        y: e.pageY - 7
+      });
+    }
+
+    // Hover an element
+    function onMouseHover() {
+      gsap.to($bigBall, {
+        duration: 0.3,
+        scale: 4
+      });
+    }
+
+    function onMouseHoverOut() {
+      gsap.to($bigBall, {
+        duration: 0.3,
+        scale: 1
+      });
+    }
+  }, []); // Run once after component is mounted
+
+  return (
+    <>
+      <div className="cursor">
+        <div className="cursor__ball cursor__ball--big">
+          <svg height="30" width="30">
+            <circle cx="15" cy="15" r="12" strokeWidth="0"></circle>
+          </svg>
+        </div>
+
+        <div className="cursor__ball cursor__ball--small">
+          <svg height="10" width="10">
+            <circle cx="5" cy="5" r="4" strokeWidth="0"></circle>
+          </svg>
+        </div>
+      </div>
+    </>
+  );
+}
