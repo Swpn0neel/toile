9e4b60b9150ff9940cc13e91628de772043f18c0
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-10-28 23:28:07 +0530

    (fix): Custom Cursor

--- src/components/Cursor/Cursor.jsx
+++ src/components/Cursor/Cursor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import "./Cursor.css";
 
@@ -8,42 +8,51 @@ export default function Cursor() {
     const $smallBall = document.querySelector('.cursor__ball--small');
     const $hoverables = document.querySelectorAll('.hoverable');
 
-    // Listeners
     document.body.addEventListener('mousemove', onMouseMove);
     for (let i = 0; i < $hoverables.length; i++) {
       $hoverables[i].addEventListener('mouseenter', onMouseHover);
       $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
     }
 
-    // Move the cursor
     function onMouseMove(e) {
       gsap.to($bigBall, {
-        duration: 0.4,
-        x: e.pageX - 15,
-        y: e.pageY - 15
+        duration: 0.6,
+        x: e.clientX - 15,
+        y: e.clientY - 15
       });
       gsap.to($smallBall, {
-        duration: 0.1,
-        x: e.pageX - 5,
-        y: e.pageY - 7
+        duration: 0.15,
+        x: e.clientX - 5,
+        y: e.clientY - 7,
+        overwrite: "auto"
       });
     }
 
-    // Hover an element
     function onMouseHover() {
       gsap.to($bigBall, {
-        duration: 0.3,
-        scale: 4
+        duration: 0.4,
+        scale: 4.5
       });
     }
 
     function onMouseHoverOut() {
       gsap.to($bigBall, {
-        duration: 0.3,
+        duration: 0.4,
         scale: 1
       });
     }
-  }, []); // Run once after component is mounted
+
+    gsap.set($bigBall, {
+      x: 15,
+      y: 15
+    });
+
+    gsap.set($smallBall, {
+      x: 10,
+      y: 10
+    });
+    
+  }, []);
 
   return (
     <>
