3deb353add0882a933b9775a79835abfc0f8f8d4
Arindam Majumder <arindammajumder2020@gmail.com>
2023-11-01 19:40:35 +0530

    (test): API Error Resolved

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -3,8 +3,8 @@ import { Client, Account } from "appwrite";
 export const API_ENDPOINT = import.meta.env.VITE_APPWRITE_URL;
 export const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
 
-console.log("API_ENDPOINT", API_ENDPOINT);
-console.log("PROJECT_ID", PROJECT_ID);
+// console.log("API_ENDPOINT", API_ENDPOINT);
+// console.log("PROJECT_ID", PROJECT_ID);
 
 const client = new Client().setEndpoint(API_ENDPOINT).setProject(PROJECT_ID);
 
