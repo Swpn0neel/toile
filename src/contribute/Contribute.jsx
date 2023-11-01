e878f2d1d940309d0be2b00d430b2e3c5b60efc1
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-11-01 14:38:16 +0530

    (Feat): Added Add icon to Contribution page

--- src/contribute/Contribute.jsx
+++ src/contribute/Contribute.jsx
@@ -18,18 +18,18 @@ export default function Contribute(){
                     <div className="sborder2 flex basis-5/12 items-end pb-16 pl-20">
                         <p className="fontlg hoverable text-6xl text-white">Contribute</p>
                     </div>
-                    <div className="flex basis-7/12 flex-col items-center justify-center gap-8">
-                        <div className="hoverable"></div>
+                    <div className="flex basis-7/12 flex-col items-center justify-center gap-3">
+                        <div className="hoverable"><img src="/assets/plus.svg" className="h-8"></img></div>
                         <div className="text-[#D9D9D950] transition-all duration-500 hover:text-[#D9D9D9]">[ADD YOUR IMAGE/]</div>
                     </div>
                 </div>
                 <div className="flex basis-1/4 flex-col">
-                    <div className="sborder2 flex basis-1/3 flex-col items-center justify-center">
-                        <div className="hoverable"></div>
+                    <div className="sborder2 flex basis-1/3 flex-col items-center justify-center gap-3">
+                        <div className="hoverable"><img src="/assets/plus.svg" className="h-8"></img></div>
                         <div className="text-[#D9D9D950] transition-all duration-500 hover:text-[#D9D9D9]">[ADD THE TITLE/]</div>
                     </div>
-                    <div className="sborder2 flex basis-1/2 flex-col items-center justify-center">
-                        <div className="hoverable"></div>
+                    <div className="sborder2 flex basis-1/2 flex-col items-center justify-center gap-3">
+                        <div className="hoverable"><img src="/assets/plus.svg" className="h-8"></img></div>
                         <div className="text-[#D9D9D950] transition-all duration-500 hover:text-[#D9D9D9]">[ADD DESCRIPTION/]</div>
                     </div>
                     <div className="hoverable flex basis-1/6 items-center justify-center bg-[#D9D9D9] text-[#060B19]">ADD+</div>
