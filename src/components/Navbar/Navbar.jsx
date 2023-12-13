67a2c1c33bb31aeb18072d09f28c6be29d804faf
Swapnoneel Saha <swapnoneelsaha111@gmail.com>
2023-12-14 01:06:16 +0530

    (fix): Minor Navbar issues

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -58,29 +58,29 @@ export default function Navbar() {
           </Link>
         )}
         {/* hidden navbar */}
-        <div className="flex sm:hidden hoverable justify-end custom2 basis-3/5">
-          <div className="p-5">
+        <div className="flex sm:hidden hoverable justify-end items-center custom2 basis-3/5">
+          <div className="pr-7">
             {isOpen ? (
-              <GiHamburgerMenu onClick={toggleNavbar} className="text-3xl" />
+              <RxCross2 onClick={toggleNavbar} className="text-4xl text-[#D9D9D9]" />
             ) : (
-              <RxCross2 onClick={toggleNavbar} className="text-4xl" />
+              <GiHamburgerMenu onClick={toggleNavbar} className="text-3xl text-[#D9D9D9]" />
             )}
           </div>
         </div>
         {isOpen ? (
           <>
             <div className="absolute w-screen mt-20 z-10 h-[90vh] bg-[#060B19]">
-              <div className="pr-10 hover:text-[#d9d9d9] transition-all duration-100">
+              <div className="text-[#D9D9D9] hover:text-[#d9d9d9] transition-all duration-100">
                 <a href="#home" className="hover:cursor-none">
                   HOME
                 </a>
               </div>
-              <div className=" hover:text-[#d9d9d9] transition-all duration-100">
+              <div className="text-[#D9D9D9] hover:text-[#d9d9d9] transition-all duration-100">
                 <a href="#about" className="hover:cursor-none">
                   ABOUT
                 </a>
               </div>
-              <div className=" hover:text-[#d9d9d9] transition-all duration-100">
+              <div className="text-[#D9D9D9] hover:text-[#d9d9d9] transition-all duration-100">
                 <a href="#gallery" className="hover:cursor-none">
                   GALLERY
                 </a>
